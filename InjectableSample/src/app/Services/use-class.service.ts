f3a783c964dc945e91fb222d320a490adf20b5c7
Manfred Stocker <manfred.stocker@jetticket.at>
2018-12-18 10:50:11 +0100

    Add providers to seClassService.spec

--- InjectableSample/src/app/Services/use-class.service.ts
+++ InjectableSample/src/app/Services/use-class.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 
+// Wird im Unittest verwendet
+export class UseClassTestService {
+  log(): string {
+    return 'Hello from UseClassTestService';
+  }
+}
+
 export class UseClassService2 {
   log(): string {
     return 'Hello from UseClassService2';
